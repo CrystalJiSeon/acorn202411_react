0c89b46a12c18f2eeba7b12651c39d3f93e14c49
kimgura <oli999@naver.com>
2025-03-14 17:42:06 +0900

    [Final 예제]  User 관련기능 구현중...

--- Step06_SPA/index.js
+++ Step06_SPA/index.js
@@ -15,7 +15,7 @@ import axios from 'axios';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} baseURL="/spring12/"/>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
